047020d96983d7e0e0b90043fd41d8d01d0e66b8
joke111 <alta.second@gmail.com>
2016-11-29 21:45:55 +0900

    kanban board v1

--- react-chapter4/src/components/App.js
+++ react-chapter4/src/components/App.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import KanbanBoard from './KanbanBoard';
+
+let cardsList = [
+    {
+        id: 1,
+        title: "Read the Book",
+        description: "I should read the whole book",
+        status: "in-progress",
+        tasks: []
+    },
+    {
+        id: 2,
+        title: "Write some code",
+        description: "Code along with the samples in the book",
+        status: "todo",
+        tasks: [
+            {
+                id: 1,
+                name: "ContactList Example",
+                done: true
+            },
+            {
+                id: 2,
+                name: "Kanban Example",
+                done: false
+            },
+            {
+                id: 3,
+                name: "My own experiments",
+                done: false
+            }
+        ]
+    },
+];
 
 class App extends React.Component {
 	render(){
-
 		return (
-			<h1>Hello React Skeleton</h1>
+		    <KanbanBoard cards={cardsList} />
 		);
 	}
 }
